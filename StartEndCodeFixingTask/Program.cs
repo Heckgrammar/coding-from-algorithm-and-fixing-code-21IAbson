92a77cde10a45ff1c8fea1cc2cfe639688ad98a1
Isobelle Abson <21IAbson@heckgrammar.co.uk>
2025-03-15 10:25:34 +0000

    Program

--- StartEndCodeFixingTask/Program.cs
+++ StartEndCodeFixingTask/Program.cs
@@ -9,6 +9,35 @@
             // Show the test outputs for the test table in the Readme
             // Put your evidence where it says Do the coding task in C#
 
+            int difference;
+            int startYear;
+            int endyear;
+            bool validChoice = false;
+            do
+            {
+                Console.WriteLine("Enter a start year");
+                startYear = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter an end year");
+                endyear = Convert.ToInt32(Console.ReadLine());
+                if (startYear >= endyear)
+                {
+                    Console.WriteLine("Start year must be before end year");
+                }
+                else if (startYear >= 2000)
+                {
+                    Console.WriteLine("Start year must be below 2000");
+                }
+                else
+                {
+                    validChoice = true;
+                }
+                
+            } while (validChoice != true);
+
+            difference = endyear - startYear;
+            Console.WriteLine(difference);
+
+
 
         }
     }
